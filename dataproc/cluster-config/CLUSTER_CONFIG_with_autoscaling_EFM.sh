92f086570027ef41866f72d2c4072372536f1dd6
datasherlock <jerome.rajan1987@gmail.com>
2022-11-09 01:07:14 +0530

    Modified image version

--- dataproc/cluster-config/CLUSTER_CONFIG_with_autoscaling_EFM.sh
+++ dataproc/cluster-config/CLUSTER_CONFIG_with_autoscaling_EFM.sh
@@ -1,7 +1,7 @@
 # !/bin/bash
 
 # Datproc Image Version
-export DATAPROC_IMAGE_VERSION='2.0-debian10'
+export DATAPROC_IMAGE_VERSION='2.0.34-debian10'
 
 
 # OPTIONAL. Leave empty if not required
@@ -9,7 +9,7 @@ export DATAPROC_OPTIONAL_COMPONENTS='JUPYTER,PRESTO'
 export DATAPROC_SCALING_POLICY='demo-scaling'
 export DATAPROC_METASTORE='' 
 export DATAPROC_INITIALISATION_SCRIPTS='gs://datasherlock/dataproc-init-scripts/change_to_local_ssd.sh'  # comma delimited list of scripts e.g. gs://'${GCS_BUCKET}'/dataproc/initialisation_actions/change_to_local_ssd.sh
-export DATAPROC_PROPERTIES='dataproc:efm.spark.shuffle=primary-worker' # comma delimited list of scripts
+export DATAPROC_PROPERTIES='dataproc:efm.spark.shuffle=primary-worker,dataproc:dataproc.monitoring.stackdriver.enable=true,dataproc:dataproc.logging.stackdriver.job.driver.enable=true,dataproc:dataproc.logging.stackdriver.job.yarn.container.enable=true' # comma delimited list of scripts
 
 export MASTER_MACHINE_TYPE='n2d-standard-2'
 export MASTER_BOOT_DISK_GB=100
--- dataproc/data-generators/pyspark_demo.py
+++ dataproc/data-generators/pyspark_demo.py
@@ -2,6 +2,7 @@
 
 """
  Sample command for Dataproc - gcloud dataproc jobs submit pyspark --cluster poc-cluster-efm --region us-central1 gs://bucket/code/pyspark_demo.py -- 10 gs://bucket/object
+ Generates a dataframe with schema as ["firstname","middlename","lastname","dob","gender","salary", "age"] and writes to specified path
 """
 
 from pyspark.sql import SparkSession
@@ -11,11 +12,20 @@ from datetime import timedelta
 import random, string
 from datetime import datetime
 import sys
+from pyspark.sql.types import *
+import math
+
   
+spark = SparkSession.builder \
+        .appName("my-demo-app") \
+        .getOrCreate()
 
 def generate_random_string(length):
     rand_string = ''.join(random.choices(string.ascii_letters + string.digits, k=length))
     return rand_string
+
+def generate_random_gender():
+    return random.choices('MF')[0]
 
 def random_date(start, end):
     """
@@ -29,30 +39,38 @@ def random_date(start, end):
 
 def generate_df(rows):
     # Create SparkSession 
-    data=[]
     d1 = datetime.strptime('1/1/1900 1:30 PM', '%m/%d/%Y %I:%M %p')
     d2 = datetime.strptime('1/1/2009 4:50 AM', '%m/%d/%Y %I:%M %p')
-    columns = ["firstname","middlename","lastname","dob","gender","salary"]
-    for i in range(1,rows):
-        data.append((generate_random_string(10), \
-                  generate_random_string(3), \
-                  generate_random_string(5), \
-                  random_date(d1, d2), \
-                  random.choices('MF')[0], \
-                  random.randint(-10,10000000) \
-                 ))
-    return spark.createDataFrame(data=data, schema = columns)
+    columns = StructType([StructField('firstname', StringType(), False),
+            StructField('middlename', StringType(), False),
+            StructField('lastname', StringType(), False),
+            StructField('dob', StringType(), False),
+            StructField('gender', StringType(), False),
+            StructField('salary', IntegerType(), False)])
 
-spark = SparkSession.builder \
-        .appName("my-demo-app") \
-        .getOrCreate() 
+    df = spark.range(1, rows)
+    generate_random_string_udf = udf(lambda x: generate_random_string(x))
+    random_date_udf = udf(lambda x,y: random_date(x,y), DateType())
+    generate_random_gender_udf = udf(lambda x : generate_random_gender())
+
+    df = df.withColumn('firstname',generate_random_string_udf(lit(10))) \
+        .withColumn('middlename',generate_random_string_udf(lit(3))) \
+            .withColumn('lastname',generate_random_string_udf(lit(5))) \
+                .withColumn('dob',random_date_udf(lit(d1), lit(d2))) \
+                    .withColumn('gender',generate_random_gender_udf(lit(0))) \
+                        .withColumn('salary', rand(1)*1000000)
+    return df
+
+
 
  
 
 rows = int(sys.argv[1])
-path = sys.argv[2]
+date = datetime.strptime(str(datetime.now()), '%Y-%m-%d %H:%M:%S.%f')
+suffix = str(date.year) + str(date.month) + str(date.day) + str(date.hour) + str(date.minute) + str(date.second)
+path = sys.argv[2] + "/" + suffix
 print("Generating " + str(rows) + " Rows")
 df = generate_df(rows)
 df_age = df.withColumn("age", months_between(current_date(),to_timestamp("dob"))/12)
-df_age.write.csv(path)
+df_age.write.csv(path, compression="gzip")
 print("Created " + str(rows) + " Rows at " + path)
